6ec9b238e9fcd5876620c701a2335d4e839707e2
FaisalABR <faisalabubakar.works@gmail.com>
2026-05-23 10:52:03 +0700

    Update resume link in Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -90,7 +90,7 @@ export default function Hero() {
           <p className={styles.bio}>
             Hi, I'm Faisal Abu Bakar Riza - a <span className={styles.highlight}>Software Engineer</span> passionate about creating seamless digital experiences that connect and convert.
           </p>
-          <a href="https://drive.google.com/file/d/1nwzwPoOfiF23zOpp5-_AWSQ8aB61O7N8/view?usp=sharing" className={styles.button}>
+          <a href="https://drive.google.com/file/d/1Ij9h3YLwEYe72_j4iJ5JVeDORi8FKqKB/view?usp=drive_link" className={styles.button}>
             <File size={20} />
             Resume
           </a>
